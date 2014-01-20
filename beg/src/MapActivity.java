54ff11439e3ebbcf36259d38f7f8e7cc296152cf
be-G <beg.developers@gmail.com>
2014-01-20 21:58:20 +0100

    Add all project
    add all project start to work to login activity

---
+++ beg/src/MapActivity.java
@@ -0,0 +1,9 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MapActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
